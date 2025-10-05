0470ee110ac8bb5cdfe82a33a178056cc2c36328
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-05 11:42:08 +0300

    Fix typo in urls.py

--- souq/urls.py
+++ souq/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-# استورد الـ Views الجديدة
-from store.views import IndexView, ProductDetailView, SignupView, LoginView, CartView # <-- أضف CartView
+
+# استيراد كل الـ Views الخاصة بصفحات الـ HTML
+from store.views import IndexView, ProductDetailView, SignupView, LoginView, CartView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # روابط الـ API (تبقى كما هي)
+    # روابط الـ API (كل ما يبدأ بـ /api/ يتم توجيهه إلى store.urls)
     path('api/', include('store.urls')),
-    path('login/', LoginView.as_view(), name='login'),
-    # روابط صفحات الـ HTML الجديدة
+
+    # روابط صفحات الـ HTML
     path('', IndexView.as_view(), name='index'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
     path('cart/', CartView.as_view(), name='cart'),
 ]
