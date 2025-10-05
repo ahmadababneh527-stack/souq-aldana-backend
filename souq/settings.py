979ec8f3a5768d8eaea2b7d4a6ce90c4ff9eabcc
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-05 11:56:04 +0300

    Add custom domain

--- souq/settings.py
+++ souq/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-default-key-for-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onrender.space', 'www.onrender.space']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
