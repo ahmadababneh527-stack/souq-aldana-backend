79101bf50f64551adfed0ed42561d718a03ec453
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-05 15:44:55 +0300

    Configure media file serving for production

--- souq/urls.py
+++ souq/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-# استيرادات جديدة
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-from store.views import IndexView, ProductDetailView, SignupView, LoginView, CartView, ProfileView # <-- أضف ProfileView
+# استيراد جديد لعرض الملفات
+from django.views.static import serve 
+from store.views import IndexView, ProductDetailView, SignupView, LoginView, CartView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,8 +13,9 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+
+    # --- السطر الجديد والمهم لعرض الصور في بيئة الإنتاج ---
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# سطر جديد لإضافة روابط الوسائط في وضع التطوير
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ملاحظة: لقد قمنا بإزالة السطر القديم الخاص بالـ static files لأنه لم يعد ضروريًا بهذه الطريقة
