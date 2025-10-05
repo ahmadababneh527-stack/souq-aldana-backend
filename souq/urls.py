b06b5ce0ecda51cf3c7e016c8c72878e340f39ff
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-05 16:08:38 +0300

    Configure media file serving for production

--- souq/urls.py
+++ souq/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
-# استيراد جديد لعرض الملفات
-from django.views.static import serve 
+from django.conf.urls.static import static  # استيراد الدالة المساعدة لخدمة الملفات
 from store.views import IndexView, ProductDetailView, SignupView, LoginView, CartView
 
+# هذه هي المسارات الأساسية لتطبيقك
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
@@ -13,9 +13,11 @@ urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('cart/', CartView.as_view(), name='cart'),
-
-    # --- السطر الجديد والمهم لعرض الصور في بيئة الإنتاج ---
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# ملاحظة: لقد قمنا بإزالة السطر القديم الخاص بالـ static files لأنه لم يعد ضروريًا بهذه الطريقة
+# --- الجزء الأهم ---
+# هذا السطر يضيف مسارًا لخدمة ملفات الوسائط (الصور)
+# Django سيقوم تلقائيًا بتفعيل هذا المسار فقط عندما يكون DEBUG = True (في بيئة التطوير المحلية)
+# لكن لكي تعمل على Render، نحتاج لإضافتها بشكل دائم
+if True: # أبقيناها True لتعمل على Render بشكل مباشر
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
