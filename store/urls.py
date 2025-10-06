044e89e31a57e16365fc40f3b9fe16fadc064877
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-06 12:45:51 +0300

    Fix import error in store URLs

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,26 @@
-# في ملف store/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    ProductViewSet, 
+    UserViewSet, 
+    LoginAPIView, 
+    CartViewSet, 
+    AddToCartAPIView, 
+    CartItemViewSet, 
+    ProfileAPIView,
+    ReviewListCreateAPIView # <-- هذا هو الاسم الصحيح
+)
 
-from django.urls import path
-from .views import IndexView, ProductDetailView, SignupView, LoginView, CartView, ProfileView, ReviewListCreateAPIView
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'cart-items', CartItemViewSet, basename='cart-item')
 
-# هذا الملف يحتوي فقط على روابط صفحات الويب (HTML)
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('cart/', CartView.as_view(), name='cart'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('', include(router.urls)),
+    path('login/', LoginAPIView.as_view(), name='api-login'),
+    path('add-to-cart/', AddToCartAPIView.as_view(), name='add-to-cart'),
+    path('profile/', ProfileAPIView.as_view(), name='api-profile'),
     path('products/<int:product_id>/reviews/', ReviewListCreateAPIView.as_view(), name='product-reviews'),
-
 ]
