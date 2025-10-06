b91a0254c77509e2b30aa8a734eb22c63862c425
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-06 12:59:26 +0300

    Fix URL config and remove redundant api_urls.py

--- souq/urls.py
+++ souq/urls.py
@@ -1,23 +1,35 @@
-# في ملف souq/urls.py
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-from store.views import IndexView, ProductDetailView, SignupView, LoginView, CartView, ProfileView, TermsView, PrivacyView # <-- أضف TermsView و PrivacyView
+from django.views.static import serve
+from store.views import (
+    IndexView, 
+    ProductDetailView, 
+    SignupView, 
+    LoginView, 
+    CartView, 
+    ProfileView, 
+    TermsView, 
+    PrivacyView
+)
 
 urlpatterns = [
+    # رابط لوحة التحكم
     path('admin/', admin.site.urls),
     
-    # أي طلب يبدأ بـ /api/ سيتم توجيهه لملف الروابط الخاص بالـ API
-    path('api/', include('store.api_urls')), 
-    
-    # أي طلب آخر (لصفحات الويب) سيتم توجيهه لملف الروابط الخاص بالتطبيق
-    path('', include('store.urls')), 
+    # كل روابط الـ API يتم توجيهها من هنا إلى ملف store/urls.py
+    path('api/', include('store.urls')),
+
+    # روابط صفحات الموقع (HTML)
+    path('', IndexView.as_view(), name='index'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('terms/', TermsView.as_view(), name='terms'),
     path('privacy/', PrivacyView.as_view(), name='privacy'),
-]
 
-# هذا الكود لخدمة ملفات الصور أثناء التطوير وفي بيئة Render
-if True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # لعرض ملفات الصور المرفوعة في بيئة الإنتاج
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
