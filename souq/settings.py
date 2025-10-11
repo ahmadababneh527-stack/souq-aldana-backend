d11078244ec830d3734532bbaaf84818639f9504
ahmadababneh327-stack <AHMADABABNEH527@GMAIL.com>
2025-10-11 22:05:19 +0300

    Rebuild all migrations to fix Review model

--- souq/settings.py
+++ souq/settings.py
@@ -132,3 +132,12 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
